62867c4627dc462866a9c4ec657664b27724f848
GotW <65318026+GeekOfTheWild@users.noreply.github.com>
2023-04-22 06:19:52 +0000

    Add index

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 debug:
-	g++ src/main.cpp -o target/Debug/main 
+	g++ src/main.cpp -o target/Debug/main -pthread
 	./target/Debug/main
 pch:
 	g++ -O0 src/deps/cpp-httplib/httplib.h 
+client:
+	g++ src/main.cpp -o target/Debug/main -pthread
+	g++ src/client.cpp -o target/Debug/client -pthread
+	./target/Debug/main
--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,20 @@ int main()
         res.set_content("Hello World!", "text/plain");
 });
 
+    // Server index page
+    server.Get("/", [&](const httplib::Request &req, httplib::Response &res){
+        res.set_content("Hello World!", "text/html");
+    });
+
+    // Stop server when user accesses /stop
+    server.Get("/stop", [&](const httplib::Request &, httplib::Response &res) {
+        server.stop();
+        res.set_redirect("/");
+    });
+
     server.listen("0.0.0.0", 8080);
+
+
 
     std::cout << "Hello World" << std::endl;
 
